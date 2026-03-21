785497c6209024f9082cede318918852b6cfe7ba
upesnavneet <kumarnavneet7765@gmail.com>
2026-03-21 20:15:50 +0530

    q8

---
+++ 8_Strig_to_int.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0, n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') 
+        {
+            i++;
+        }
+
+        int sign = 1;
+        if (i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')) 
+        {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        int sum = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) 
+        {
+            int digit = s.charAt(i) - '0';
+
+            if (sum > Integer.MAX_VALUE / 10 || 
+               (sum == Integer.MAX_VALUE / 10 && digit > 7)) 
+            {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            sum = sum * 10 + digit;
+            i++;
+        }
+
+        return sum * sign;
+    }
+}
